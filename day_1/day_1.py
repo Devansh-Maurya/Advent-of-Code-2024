4ecc88a8ad5dc3a47f55f6d8cb8b8191fb505a5e
Devansh Maurya <Devansh233@gmail.com>
2024-12-04 07:51:47 +0000

    Add day 1 part 1 and part 2 solutions in day_1.py

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,4 +1,5 @@
 import fileinput
+from collections import Counter
 
 locations = []
 
@@ -8,9 +9,18 @@ for line in fileinput.input("test"):
 l1 = sorted([location[0] for location in locations])
 l2 = sorted([location[1] for location in locations])
 
-ans = 0
+# ---- Part 1 ----
+ansPart1 = 0
 
 for i in range(len(l1)):
-    ans += abs(l1[i] - l2[i])
+    ansPart1 += abs(l1[i] - l2[i])
 
-print(ans)
+print(ansPart1)
+
+# ---- Part 2 ----
+l1Counts = Counter(l1)
+l2Counts = Counter(l2)
+
+ansPart2 = sum([ k * l1Counts[k] * l2Counts[k] for k in l1Counts])
+
+print(ansPart2)
